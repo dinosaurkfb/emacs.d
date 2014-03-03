15a9eabe75da925e118677514f19713e057bad61
Kong Fanbin <kfbuniversity@gmail.com>
2014-03-03 12:00:25 +0800

    Add keybinding C-m to set-mark-command.

--- init-local.el
+++ init-local.el
@@ -3,6 +3,10 @@
 (global-set-key (kbd "M-_") 'backward-paragraph)
 (global-set-key (kbd "M-+") 'forward-paragraph)
 
+;; C-@ is used by expand-region. And C-space is always the same
+;; as C-@ when using XTERM
+(global-set-key (kbd "C-m") 'set-mark-command)
+
 (global-set-key "%" 'match-paren)
 
 (defun match-paren (arg)
@@ -11,5 +15,9 @@
   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
         ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
         (t (self-insert-command (or arg 1)))))
+
+(setq user-full-name "Kong Fanbin")
+(setq user-mail-address "kfbuniversity@gmail.com")
+;;Personal Information
 
 (provide 'init-local)
